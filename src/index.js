c3dc6aff3c12f889ce555bcb447a167bae64e1aa
sarimarton <sarimarton@gmail.com>
2018-10-23 23:39:30 +0200

    Add a barebone search input using http and time side-effects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import xs from 'xstream';
 import { run } from '@cycle/run';
 import { makeDOMDriver, div, input, p } from '@cycle/dom';
+import { makeHTTPDriver } from '@cycle/http';
+import { timeDriver } from '@cycle/time';
 import Snabbdom from 'snabbdom-pragma';
 import { routerify } from 'cyclic-router';
 import { makeHistoryDriver } from '@cycle/history';
@@ -14,10 +16,12 @@ import { ItemComponent } from './view/item/Item.js';
 function main(sources) {
   const homePageClick$ = sources.DOM.select('.home').events('click');
 
-  const match$ = sources.router.define({
+  const views = {
     '/': HomeComponent,
     '/item': ItemComponent
-  });
+  };
+
+  const match$ = sources.router.define(views);
 
   const page$ = match$.map(
     ({ path, value: page }) =>
@@ -27,18 +31,43 @@ function main(sources) {
       })
   );
 
+  const headerVDom =
+    <div>
+      header
+    </div>;
+
   return {
-    DOM: page$.map(c => c.DOM).flatten(),
-    router: xs.merge(
-      homePageClick$.mapTo('/')
-    )
+    DOM:
+      page$
+        .map(page =>
+          page.DOM.map(pageVDom =>
+            <div>
+              {headerVDom}
+              {pageVDom}
+            </div>
+          )
+        ).flatten(),
+
+    HTTP:
+      page$
+        .map(page => page.HTTP)
+        .filter(http => http)
+        .flatten(),
+
+    router:
+      xs.merge(
+        homePageClick$.mapTo('/')
+      )
   };
 }
 
 const mainWithRouting = routerify(main, switchPath);
 const drivers = {
   DOM: makeDOMDriver('#app'),
-  history: makeHistoryDriver()
+  history: makeHistoryDriver(),
+  HTTP: makeHTTPDriver(),
+  Time: timeDriver,
+  apiKey: () => 'bf6b860ab05ac2d94054ba9ca96cf1fa'
 };
 
 run(mainWithRouting, drivers);
--- src/view/home/Home.js
+++ src/view/home/Home.js
@@ -4,11 +4,65 @@ import { div } from '@cycle/dom';
 import Snabbdom from 'snabbdom-pragma';
 
 export function HomeComponent(sources) {
+  const searchPhrase$ =
+    sources.DOM
+      .select('.search-phrase-input')
+      .events('input')
+      .map(ev => ev.target.value)
+      .filter(val => val.length > 3)
+      .compose(sources.Time.debounce(250))
+      .startWith('');
+
+  const searchRequest$ =
+    searchPhrase$
+      .filter(val => val)
+      .map(searchPhrase => ({
+        url: `https://api.themoviedb.org/3/search/movie?api_key=${sources.apiKey}&query=${searchPhrase}`,
+        category: 'search',
+        isRequest: true // duck typing :(
+      }));
+
+  const searchResponse$ =
+    sources.HTTP
+      .select('search')
+      .map(resp$ =>
+        resp$.replaceError(err => xs.of(err))
+      )
+      .flatten()
+      .map(resp =>
+        resp instanceof Error
+          ? resp
+          : JSON.parse(resp.text)
+      );
+
+  const content$ =
+    searchResponse$
+      .startWith('');
+
+  const isLoading$ =
+    xs.merge(searchRequest$, searchResponse$)
+      .map(r => r && r.isRequest)
+      .startWith(false);
+
+  const isError$ =
+    xs.merge(searchRequest$, searchResponse$)
+      .map(r => r instanceof Error)
+      .startWith(false);
+
   return {
-    DOM: xs.of(
-      <div className={'page-home'}>
-        Home Page
-      </div>
-    )
+    DOM:
+      xs.combine(searchPhrase$, content$, isLoading$, isError$)
+        .map(([searchPhrase, content, isLoading, isError]) => {
+          return <div className={'page-home'}>
+            Home Page<br />
+            Search: <input className={'search-phrase-input'} />
+            <div>{isLoading ? 'Loading...' : ''}</div>
+            <div>{isError ? 'Network error...' : ''}</div>
+            {searchPhrase}
+            <pre>{JSON.stringify(content)}</pre>
+          </div>
+        }),
+
+    HTTP: searchRequest$
   };
 }
