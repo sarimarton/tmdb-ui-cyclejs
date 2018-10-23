c905d12cbffedabdc6814be644667360f961ddbe
sarimarton <sarimarton@gmail.com>
2018-10-23 16:53:09 +0200

    Add css-loader

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import switchPath from 'switch-path';
 
 import { HomeComponent } from './home/home.js';
 import { ItemComponent } from './item/item.js';
+import css from './index.css';
 
 function main(sources) {
   const homePageClick$ = sources.DOM.select('.home').events('click');
