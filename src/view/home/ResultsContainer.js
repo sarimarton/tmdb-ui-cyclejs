51c4f45ecf9327df1905395057b8fb2bb3f82c19
sarimarton <sarimarton@gmail.com>
2018-10-26 01:50:07 +0200

    Add captions to thumbnails

--- src/view/home/ResultsContainer.js
+++ src/view/home/ResultsContainer.js
@@ -1,29 +1,29 @@
 import xs from 'xstream';
 import Snabbdom from 'snabbdom-pragma';
 
+import css from './ResultsContainer.css';
+
 export function ResultsContainer(isLoading, isError, results) {
   return (
     <div className="results-container">
       <div>{isLoading ? 'Loading...' : ''}</div>
-      <div>{isError ? 'Network error...' : ''}</div>
+      <div>{isError ? 'Network error' : ''}</div>
 
       <ul className="uk-thumbnav">
       {typeof results === 'object' && !isLoading && !isError && results.map(result => {
 
         return (
           result.backdrop_path &&
-          <li className="uk-margin-bottom" attrs={{width: 315, height: 169}}>
-            <a href="#"
-              className="result-item"
-              attrs={{onclick: 'return false' }}
-              data-id={result.id}
-            >
+          <li className="uk-margin-bottom">
+            <a className="result-item" href="#" attrs={{onclick: 'return false' }} data-id={result.id}>
 
               {/* I should assemble the URL according to
                 https://developers.themoviedb.org/3/configuration/get-api-configuration
               */}
-
-              <img src={`http://image.tmdb.org/t/p/w300${result.backdrop_path}`} alt="" />
+              <div className="result-item-thumbnail-holder">
+                <img src={`http://image.tmdb.org/t/p/w300${result.backdrop_path}`} alt="" />
+              </div>
+              <div className="result-item-caption uk-text-small uk-text-muted">{result.title}</div>
             </a>
           </li>
         );
